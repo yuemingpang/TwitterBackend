bbbdb9724de90fb8de9df93abab0a2f7152ae828
Yueming Pang <pymdebaby@gmail.com>
2023-08-31 20:20:43 -0400

    update create tweet

--- src/routes/tweetRoutes.ts
+++ src/routes/tweetRoutes.ts
@@ -20,6 +20,7 @@ router.post('/', async (req, res) => {
                 image,
                 userId: user.id,
             },
+            include: {user: true},
         });
         res.json(result);
 
